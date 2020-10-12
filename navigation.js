2309c60cb8cc4ff9fbc4332a61b3b418997108ef
rhetthancock <hancockrhett@gmail.com>
2020-10-12 05:29:18 -0600

    Unified portfolio components into index.html

--- navigation.js
+++ navigation.js
@@ -1,38 +1,30 @@
-var ticking = false;
-
-function ctaf() {
-    window.scroll({
-        top: window.innerHeight,
-        left: 0,
-        behavior: 'smooth'
-    });
-}
-
-function initNav() {
-    var cta = document.getElementById("cta");
-    cta.addEventListener("click", ctaf);
-}
+let ticking = false;
 
 function checkPosition(scroll_pos) {
-    var bar = document.getElementById("bar");
+    let logo = document.getElementById("logo");
     if(scroll_pos >= window.innerHeight - 150) {
-        bar.classList.add("active");
+        logo.classList.add("active");
     }
     else {
-        bar.classList.remove("active");
+        logo.classList.remove("active");
     }
-    console.log("AH");
 }
 
-window.addEventListener("load", initNav);
 window.addEventListener("scroll", (event) => {
-    var scroll_pos = window.scrollY;
+    let scroll_pos = window.scrollY;
     if(!ticking) {
         window.requestAnimationFrame(function() {
-            
             checkPosition(scroll_pos);
             ticking = false;
         });
         ticking = true;
     }
+});
+
+window.addEventListener('load', function() {
+    let nav = document.getElementById('nav');
+    let toggle = document.getElementById('nav-toggle');
+    toggle.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
 });
